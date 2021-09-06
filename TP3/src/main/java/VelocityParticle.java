8eb31909ce67a7fd59829a2ea24759d9819adf51
Matías <mbaiges@itba.edu.ar>
2021-09-06 12:02:08 -0300

    fix: changed Velocity Particle model

--- TP3/src/main/java/VelocityParticle.java
+++ TP3/src/main/java/VelocityParticle.java
@@ -1,33 +1,39 @@
 public class VelocityParticle extends Particle {
 
-    private Double speed;
-    private Double angle;
+    private Double vx;
+    private Double vy;
 
     public VelocityParticle(Particle particle, Double speed, Double angle) {
-        super(particle.getId(), particle.getX(), particle.getY(), particle.getRadius());
-        this.speed = speed;
-        this.angle = angle;
+        this(particle.getId(), particle.getX(), particle.getY(), particle.getRadius(), speed, angle);
     }
 
     public VelocityParticle(long id, double x, double y, double radius, Double speed, Double angle) {
         super(id, x, y, radius);
-        this.speed = speed;
-        this.angle = angle;
+        this.vx = speed * Math.cos(angle);
+        this.vy = speed * Math.sin(angle);
     }
 
     public Double getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(Double speed) {
-        this.speed = speed;
+        return Math.sqrt(Math.pow(vx, 2) + Math.pow(vy, 2));
     }
 
     public Double getAngle() {
-        return angle;
+        return (vx == 0)? ((vy >= 0)? 0: Math.PI): Math.tan(vy/vx);
     }
 
-    public void setAngle(Double angle) {
-        this.angle = angle;
+    public Double getVx() {
+        return vx;
+    }
+
+    public void setVx(Double vx) {
+        this.vx = vx;
+    }
+
+    public Double getVy() {
+        return vy;
+    }
+
+    public void setVy(Double vy) {
+        this.vy = vy;
     }
 }
