56da31a49f4ef25402a39190ecac20cfe671039e
eugepineiro <epineiro@itba.edu.ar>
2021-09-12 10:50:17 -0300

    started postprocessing

--- TP3/postprocessing/main.py
+++ TP3/postprocessing/main.py
@@ -1,5 +1,5 @@
 import json, random, sys
-from plotter import plot_times_pdf
+from plotter import plot_probability_distribution
 
 with open("../src/main/resources/config/config.json") as f:
     config = json.load(f) 
@@ -9,7 +9,6 @@ with open("../src/main/resources/postprocessing/SdS_TP3_2021Q2G01_results.json")
 
 
 number_of_particles = config["n_number_of_particles"]
-M_grid_size = config["m_grid_dimension"]
 L_grid_side = config["l_grid_side"]  
 frames = config["frames"]
 
@@ -19,4 +18,5 @@ avg_times = []
 for i in range(len(results)-1): 
     avg_times.append( results[i+1] - results[i])
 
-plot_times_pdf(avg_times)
+plot_probability_distribution(avg_times, 'time')
+#plot_probability_distribution(speeds, 'speed')
--- TP3/postprocessing/plotter.py
+++ TP3/postprocessing/plotter.py
@@ -1,14 +1,51 @@
 import plotly.express as px
 import plotly.graph_objects as go
+import numpy as np
 
-def plot_times_pdf(times): 
+def plot_probability_distribution(data, var): 
+    
+    #data = [ {time: [1,2,3], n: 100}, {time: [1,2,3], n: 125}] o directamente pasarle un array solo con los times y otro solo con los N
 
-    fig = go.Figure(data=[go.Histogram(x=times)]) #TODO esto tiene que ser scatter
-    fig.update_traces(opacity=0.75)
+    fig = go.Figure()
+
+    for i in range(len(data)): # por cada valor de N
+
+        #esto deberia ser un scatter 
+        fig.add_trace(go.Histogram(
+            x=data[var], histnorm='probability',
+            xbins=dict(start=np.min(data), 
+                    end=np.max(data)),
+            marker=dict(color='rgb(5, 25, 100)'))
+        )
+
+    #fig.add_trace(go.Scatter( 
+    #    x=list( dict.fromkeys(times )), 
+    #    y=get_probability_distribution(times), 
+    #    mode='markers',           
+    #)
+    #)
+#
+    #get_probability_distribution([1,1,2,2,4, 0])
+#
     fig.update_layout(
-        title="Times PDF",
-        xaxis_title="Time",
-        yaxis_title="Number of Times Between Collisions"
+    title="Times Probability Distribution",
+    xaxis_title="Time",
+    yaxis_title="Probability",
+    legend_title=f"References\n",
+    font=dict( 
+        size=20, 
+        )
     )
    
     fig.show()
+
+def get_probability_distribution(data): #tiene que haber ya una funcion de python que lo haga solo.. y se le pasa al eje y del scatter
+
+    probabilities = []
+    data_set = list( dict.fromkeys(data) )
+
+    for n in data_set: 
+        probabilities.append(data.count(n)/len(data))
+ 
+    return probabilities
+         
