6749c443b2b023f82e5faf56933b7aa787d20e10
Matías <mbaiges@itba.edu.ar>
2021-09-26 19:04:41 -0300

    feat: added mati mse

--- TP4/postprocessing/oscillator/main.py
+++ TP4/postprocessing/oscillator/main.py
@@ -1,5 +1,11 @@
 import json, math
+import numpy as np
+
 from plotter import plot_oscillator_position_by_time
+
+def mse_mati(positions):
+    pos = np.array(positions)
+    return sum(np.power(pos[:len(positions)-1] - pos[1:], 2))/len(pos[1:])
 
 def mse(positions):
     diferences = []
@@ -13,9 +19,17 @@ with open("../../src/main/resources/postprocessing/SdS_TP4_2021Q2G01_oscillator_
 
 print(len(oscillator_results))
 
-positions = list(map(lambda p: p['particles'][0]['y'], oscillator_results))
-times = list(map(lambda t: t['time'], oscillator_results))
+positions = list(map(
+    lambda res: list(map(lambda p: p['particles'][0]['y'], res['results'])),
+    oscillator_results
+))
+names = list(map(
+    lambda res: ' '.join(list(map(lambda w: w.capitalize(), res['integration'].split('_')))),
+    oscillator_results
+))
+times = list(map(lambda t: t['time'], oscillator_results[0]['results']))
 
-print(mse(positions))
+print(mse_mati(positions[0]))
+print(mse(positions[0]))
 
-plot_oscillator_position_by_time([positions], times, ['Verlet Original'])
+plot_oscillator_position_by_time(positions, times, names)
