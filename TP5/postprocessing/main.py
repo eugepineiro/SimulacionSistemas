b49194178fd37d8117288111232b604780da29b9
eugepineiro <epineiro@itba.edu.ar>
2021-10-27 23:49:57 -0300

    added evacuated particles by time plot

--- TP5/postprocessing/main.py
+++ TP5/postprocessing/main.py
@@ -1,9 +1,39 @@
 import json
-from plotter import plot_evacuated_particles_by_time
+import numpy as np
+from plotter import plot_evacuated_particles_by_time,plot_avg_times_by_evacuated_particles
 
-with open("../../src/main/resources/config/config.json") as f:
-    config = json.load(f)
+with open("../src/main/resources/postprocessing/SdS_TP5_2021Q2G01_multiple_simulations_results.json") as f:
+    multiple_simulations_results = json.load(f)
 
-with open("../../src/main/resources/postprocessing/SdS_TP4_2021Q2G01_results.json") as f:
-    results = json.load(f)
+# Exercise a
 
+evacuated_particles_by_simulation = []
+
+dt = multiple_simulations_results[0]['dt']
+max_t = max(list(map( lambda sim: sim['escapeTimes'][-1], multiple_simulations_results)))
+print(max_t)
+
+times = list(np.arange(0, max_t+dt, step=dt))
+
+evacuated_particles_by_simulation = list(map( 
+    lambda sim: list(map(
+        lambda t: len(
+            list(filter(lambda et: et <= t, sim['escapeTimes']))
+        ), times
+    )), multiple_simulations_results
+))
+
+# for simulation in multiple_simulations_results:
+#     print(simulation['escapeTimes'][-1])
+
+# print(times)
+# print(evacuated_particles_by_simulation)
+
+plot_evacuated_particles_by_time(evacuated_particles_by_simulation, times)
+
+# Exercise b
+
+evacuated_particles_by_simulation_np = np.array(evacuated_particles_by_simulation)
+mean = np.mean(evacuated_particles_by_simulation_np, axis=1)
+std = np.std(evacuated_particles_by_simulation_np, axis=1)
+plot_avg_times_by_evacuated_particles(mean, times)
--- TP5/postprocessing/plotter.py
+++ TP5/postprocessing/plotter.py
@@ -6,18 +6,41 @@ def plot_evacuated_particles_by_time(evacuated_particles_by_simulation, times):
 
     fig = go.Figure()
 
-    for simulation in evacuated_particles_by_simulation:
+    for simulation_index in range(0, len(evacuated_particles_by_simulation)):
         fig.add_trace(go.Scatter(
             x=times,
-            y=evacuated_particles_by_simulation[simulation],
+            y=evacuated_particles_by_simulation[simulation_index],
             mode='lines+markers', 
-            name=f'Simulación = {simulation}'
+            name=f'Simulación = {simulation_index}'
         ))    
 
     fig.update_layout(
         title="Curva de Descarga",
-        xaxis_title="Tiempo (s)",
+        xaxis_title="Tiempo (ms)",
         yaxis_title="Número de Partículas Salientes",
+        legend_title=f"<b>Referencias</b> <br>",
+        font=dict( 
+            size=28, 
+        )
+    )
+
+    fig.show()
+
+def plot_avg_times_by_evacuated_particles(evacuated_particles, avg_times): 
+
+    fig = go.Figure()
+
+     
+    fig.add_trace(go.Scatter(
+        x=evacuated_particles,
+        y=avg_times,
+        mode='lines+markers'
+    ))    
+
+    fig.update_layout(
+        title="Comportamiento promedio del sistema ",
+        xaxis_title="Número de Partículas Salientes",
+        yaxis_title="Tiempo Promedio (ms)",
         legend_title=f"<b>Referencias</b> <br>",
         font=dict( 
             size=28, 
