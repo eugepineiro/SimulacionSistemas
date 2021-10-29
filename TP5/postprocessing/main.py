21696fe72a06dd2b102f2407f36fe316377833a9
eugepineiro <epineiro@itba.edu.ar>
2021-10-28 22:42:19 -0300

    added flow rate plot
    Co-authored-by: Matías S. Baiges <mbaiges@itba.edu.ar>
    Co-authored-by: Nicolas Comerci <ncomerci@itba.edu.ar>

--- TP5/postprocessing/main.py
+++ TP5/postprocessing/main.py
@@ -1,9 +1,12 @@
 import json
 import numpy as np
-from plotter import plot_evacuated_particles_by_time,plot_avg_times_by_evacuated_particles
+from plotter import plot_evacuated_particles_by_time, plot_avg_times_by_evacuated_particles, plot_avg_flow_rate_by_target_width
 
 with open("../src/main/resources/postprocessing/SdS_TP5_2021Q2G01_multiple_simulations_results.json") as f:
     multiple_simulations_results = json.load(f)
+
+with open("../src/main/resources/postprocessing/SdS_TP5_2021Q2G01_multiple_width_and_particles_results.json") as f:
+    multiple_width_and_particles_results = json.load(f)
 
 # Exercise a
 
@@ -11,7 +14,7 @@ evacuated_particles_by_simulation = []
 
 dt = multiple_simulations_results[0]['dt']
 max_t = max(list(map( lambda sim: sim['escapeTimes'][-1], multiple_simulations_results)))
-print(max_t)
+ 
 
 times = list(np.arange(0, max_t+dt, step=dt))
 
@@ -37,3 +40,37 @@ evacuated_particles_by_simulation_np = np.array(evacuated_particles_by_simulatio
 mean = np.mean(evacuated_particles_by_simulation_np, axis=1)
 std = np.std(evacuated_particles_by_simulation_np, axis=1)
 plot_avg_times_by_evacuated_particles(mean, times)
+
+# Exercise c
+
+def get_flow_rate(simulation):
+    escape_times = simulation['escapeTimes']
+    # first approach
+    return len(escape_times) / (escape_times[-1]*simulation['targetWidth'])
+
+target_widths = []
+number_of_particles = []
+flow_rates_by_pairs = []
+
+# [
+#     // n1, d1
+#         [// fs1
+#         // fs2
+#         // fs3
+#         ...]
+#     // n2, d2
+# ]
+
+[]
+
+for pair_simulations in multiple_width_and_particles_results:
+
+    d = pair_simulations[0]['targetWidth']
+    n = len(pair_simulations[0]['escapeTimes'])
+
+    target_widths.append(d)
+    number_of_particles.append(n)
+
+    flow_rates_by_pairs.append(list(map(lambda sim: get_flow_rate(sim), pair_simulations)))
+
+plot_avg_flow_rate_by_target_width(flow_rates_by_pairs, number_of_particles, target_widths)
--- TP5/postprocessing/plotter.py
+++ TP5/postprocessing/plotter.py
@@ -1,4 +1,5 @@
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import math
 import numpy as np
 
@@ -46,5 +47,73 @@ def plot_avg_times_by_evacuated_particles(evacuated_particles, avg_times):
             size=28, 
         )
     )
+
+    fig.show()
+
+def plot_avg_flow_rate_by_target_width(flow_rates_by_pairs, number_of_particles, target_widths): 
+
+    # fig = go.Figure()
+
+    flow_rates_np = np.array(flow_rates_by_pairs)
+    mean = np.mean(flow_rates_np, axis=1)
+    std = np.std(flow_rates_np, axis=1)
+
+    # fig.add_trace(go.Scatter(
+    #     x=number_of_particles, 
+    #     y=mean,
+    #     mode='lines+markers', 
+    #     error_y=dict(
+    #         type='data',
+    #         symmetric=True,
+    #         array=std
+    #     )
+    # )) 
+
+    fig=make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig.update_layout(xaxis2= {'anchor': 'y', 'overlaying': 'x', 'side': 'top'},yaxis_domain=[0, 0.94]) 
+    
+    fig.add_trace(go.Scatter(
+        x=target_widths, 
+        y=mean,
+        mode='lines+markers', 
+        error_y=dict(
+            type='data',
+            symmetric=True,
+            array=std
+        )
+    ), secondary_y=False) 
+
+    fig.add_trace(go.Scatter(
+        x=number_of_particles, 
+        y=mean,
+        mode='lines+markers', 
+        error_y=dict(
+            type='data',
+            symmetric=True,
+            array=std
+        )
+    ), secondary_y=False)
+
+    fig.data[0].update(xaxis='x2')
+
+    # https://plotly.com/python/reference/layout/
+    fig.update_layout(
+        title_text="Caudal Medio en función del número de particulas y el ancho de salida",
+        title_pad=dict(
+            b=20
+        ),
+        xaxis_title="Número de Partículas",
+        xaxis2_title="Ancho de Salida (m)",
+        yaxis_title="Caudal Medio (1/m/s)",
+        legend_title=f"<b>Referencias</b> <br>",
+        font=dict( 
+            size=25, 
+        )
+    )
+
+
+    # fig.update_layout(xaxis2= {'anchor': 'y', 'overlaying': 'x', 'side': 'top'}, yaxis_domain=[0, 0.94])
+    # fig.data[0].update(xaxis='x')
 
     fig.show()
