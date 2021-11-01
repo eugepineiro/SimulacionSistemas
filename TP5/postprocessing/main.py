79e51293b027e1409690443a5bafd7b03f5f078f
Matías <mbaiges@itba.edu.ar>
2021-11-01 12:58:36 -0300

    fix: updated window_size

--- TP5/postprocessing/main.py
+++ TP5/postprocessing/main.py
@@ -8,7 +8,7 @@ with open("../src/main/resources/postprocessing/SdS_TP5_2021Q2G01_multiple_simul
 with open("../src/main/resources/postprocessing/SdS_TP5_2021Q2G01_multiple_width_and_particles_results.json") as f:
     multiple_width_and_particles_results = json.load(f)
 
-WINDOW_SIZE = 5
+WINDOW_SIZE = 10
 
 # Exercise a
 
@@ -43,7 +43,12 @@ def get_flow_rates_by_time(times_by_simulation, window_size):
     flow_rates = []
     
     c = len(mean)
-    for i in range(0, c, window_size):
+    # for i in range(0, c, window_size):
+    #     n = window_size - 1 if (c - i) >= window_size else (c - i) - 1 # TODO(mati): revisar el else
+    #     escape_times.append((mean[i+n] + mean[i]) / 2)
+    #     flow_rates.append(n/(mean[i+n] - mean[i]))
+
+    for i in range(0, c - window_size):
         n = window_size - 1 if (c - i) >= window_size else (c - i) - 1 # TODO(mati): revisar el else
         escape_times.append((mean[i+n] + mean[i]) / 2)
         flow_rates.append(n/(mean[i+n] - mean[i]))
